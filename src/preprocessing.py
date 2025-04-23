fa4bfbdfb9d75bac91b20a97d4f0f7dbc7fb8904
Tommy Burkett <158101222+tommy-burkett@users.noreply.github.com>
2025-04-23 18:42:43 -0400

    commit

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,27 @@
-# Create file
+# Imports
+import pandas as pd
+import numpy as np
+import os
+
+
+def clean_data(filepath: str, 
+               year: int, 
+               side: str
+              ) -> pd.DataFrame:
+    
+    """
+        Clean the data from the given filepath.
+
+        Args:
+            filepath (str): The path to the data file.
+            year (int): The year of the data.
+            side (str): The side of the data ('left' or 'right').
+        Returns:
+            pd.DataFrame: The cleaned NFL data for dashboard.
+    """
+
+    # Read in csv, skip the section header row
+    df = pd.read_csv(filepath, skiprows=1)
+
+    return df
+
